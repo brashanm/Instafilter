2464dbd7d02d8254d6af2f3e5f80706c3cdf371a
Brashan <brashanm@gmail.com>
2023-12-20 22:16:46 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Brashan Mohanakumar on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
